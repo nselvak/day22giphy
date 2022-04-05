db2388d02c3ccf6eb6da40cf0fd57011a5764a87
nselvak <selvakumar9155@gmail.com>
2022-04-05 11:05:15 +0800

    Initial commit

---
+++ src/main/java/nus/iss/day22/giphy/GiphyApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.day22.giphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiphyApplication.class, args);
+	}
+
+}
